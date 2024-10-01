9c233f4157c13709ae669a2de6f6de40542767ab
Thang0226 <thang.nd0226@gmail.com>
2024-10-01 11:01:01 +0700

    add hard modes

--- mine-sweeper.js
+++ mine-sweeper.js
@@ -1,6 +1,25 @@
-function restart() {
-  document.location.reload();
-}
+let numRows;
+let numCols;
+let numMines;
 
-let game_board = new GameBoard(10, 10, 10, "game-board");
-game_board.initBoard();
+function startPlay() {
+  let mode = document.getElementById("mode").value;
+  switch (mode) {
+    case "easy":
+      numRows = 10;
+      numCols = 10;
+      numMines = 10;
+      break;
+    case "medium":
+      numRows = 15;
+      numCols = 20;
+      numMines = 35;
+      break;
+    case "hard":
+      numRows = 20;
+      numCols = 30;
+      numMines = 70;
+  }
+  let game_board = new GameBoard(numRows, numCols, numMines, "game-board");
+  game_board.initBoard();
+}
